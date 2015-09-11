2417ba78a291d0f4e24e1868a0fa9f76786f8839
Anton Shelkov <anton.shelkov@gmail.com>
2015-09-11 14:41:40 +0300

    Adding navigation

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,10 +4,14 @@
     <meta charset="UTF-8">
     <title>@yield('title')</title>
     <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/app.css" rel="stylesheet">
+    <script src="/js/jquery.min.js"></script>
     <script src="/js/bootstrap.min.js"></script>
+
 </head>
 <body>
-    <div class="container">
+    @include('pages.nav')
+    <div class="container" id="content">
         @yield('mainContent')
     </div>
 </body>
